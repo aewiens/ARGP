c95f36f74693ee4678b4339ae292f2d99f1a9b86
Avery Wiens <avery.wiens@gmail.com>
2018-09-05 15:25:40 -0400

    trashed old surfaces that i'm not using for examples

--- scripts/3Dexamples/old/surfaces/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-test = np.loadtxt("ccsd-t-5z.dat", delimiter=',', skiprows=1)
-
-E = test[::10, :]
-
-print(len(E))
-
-print(E[0])
--- scripts/3Dexamples/old/test-surfaces/mean.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-f = np.loadtxt("temp.dat")
-
-rmse = f[:, 1]
-print(np.mean(rmse))
-
-
