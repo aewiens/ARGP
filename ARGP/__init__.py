bda3b8f3140b60e2f3aa66667341aff2f406bf72
Avery Wiens <avery.wiens@gmail.com>
2019-01-16 17:22:46 -0500

    fixed a bug in the smart random sampling code for ARGP modeling

--- ARGP/__init__.py
+++ ARGP/__init__.py
@@ -3,5 +3,6 @@ from . import ordinary
 from . import largp
 from . import kernel
 from . import nargp
+from . import data_sampler
 
-__all__ = ['matrix', 'ordinary', 'largp', 'nargp', 'kernel']
+__all__ = ['matrix', 'ordinary', 'largp', 'nargp', 'kernel', 'data_sampler']
--- ARGP/data_sampler.py
+++ ARGP/data_sampler.py
@@ -44,25 +44,24 @@ def split(X, n_train, n_test=None, seed=None):
     return ind_train, ind_test
 
 
-def train_test_split(*arrays, n_train, n_test=None, seed=42):
+def train_test_split(*arrays, n_train, n_test=None, seed):
     arrays = indexable(*arrays)
     train, test = split(X=arrays[0], n_train=n_train, n_test=n_test, seed=seed)
     return list(chain.from_iterable((a[train], a[test]) for a in arrays))
 
 
-def train_test_split2(*arrays, ind_train_1, ind_test_1, n_train, seed=42):
+def train_test_split2(*arrays, ind_train_1, ind_test_1, n_train, seed):
 
     arrays = indexable(*arrays)
-    train_, test_ = split(ind_train_1, n_train=4)
+    train_, test_ = split(ind_train_1, n_train=n_train, seed=seed)
     train = ind_train_1[train_]
     test = np.concatenate((ind_train_1[test_], ind_test_1))
 
     return list(chain.from_iterable((a[train], a[test]) for a in arrays))
     #return X[train2], X[test2], y[train2], y[test2]
-    #return list(chain.from_iterable((a[ind_train], a[ind_test]) for a in arrays))
 
 
-def smart_random(dataset, n_train, n_test):
+def smart_random(dataset, n_train, n_test, save_to=None):
     """
     choose a random training set that has an energy distribution most resembling that of the full dataset.
     uses the chi-squared method to estimate the similarity of the energy distrubtions.
@@ -81,6 +80,10 @@ def smart_random(dataset, n_train, n_test):
     indices = np.arange(len(dataset))
     ind_train, ind_test  = train_test_split(indices, n_train=n_train, n_test=n_test, seed=best_seed)
 
+    if save_to:
+        np.savetxt(save_to + "_train.dat", ind_train)
+        np.savetxt(save_to + "_test.dat", ind_test)
+
     return ind_train, ind_test
 
 
@@ -93,16 +96,14 @@ def smart_random2(dataset, n_train, i1, i1_test):
     x, y = np.split(dataset, [-1], axis=1)
     full_dataset_dist, binedges = np.histogram(y, bins='auto', density=True)
     pvalues = []
-    for seed in range(600): 
-    #for seed in range(1):
+    for seed in range(500): 
         y_train = train_test_split2(x, y, ind_train_1=i1, ind_test_1=i1_test, n_train=n_train, seed=seed)[2]
         train_dist, tmpbin = np.histogram(y_train, bins=binedges, density=True)
         chisq, p = stats.chisquare(train_dist, f_exp=full_dataset_dist)
         pvalues.append(p)
     best_seed = np.argmax(pvalues)
     indices = np.arange(len(dataset))
-    ind_train, ind_test  = train_test_split(indices, n_train=n_train, seed=best_seed)
-
+    ind_train, ind_test  = train_test_split2(indices, ind_train_1 = i1, ind_test_1=i1_test, n_train=n_train, seed=best_seed)
     return ind_train, ind_test
 
 
@@ -111,12 +112,8 @@ if __name__ == '__main__':
     E1 = np.loadtxt("../scripts/3Dexamples/surfaces/ccsd-t-dz.dat", delimiter=',', skiprows=1)
     E2 = np.loadtxt("../scripts/3Dexamples/surfaces/ccsd-t-5z.dat", delimiter=',', skiprows=1)
 
-    i1, i1_test = smart_random(E1, 10, 100)
-    i2, i2_test = smart_random2(E2, 4, i1, i1_test)
-    """
-    #Xtrain, Ytrain, Xtest, Ytest = train_test_split2(X, Y, ind_train_1=i1, ind_test_1=i1_test, n_train_2=4, seed=1)
-    Xtrain, Xtest, Ytrain, Ytest = train_test_split2(X, Y, *smart_random(E1, 10), n_train_2=4, seed=1)
+    E1x, E1y = np.split(E1, [-1], axis=1)
+    E2x, E2y = np.split(E2, [-1], axis=1)
 
-    print(Ytrain.shape)
-    print(Ytest.shape)
-    """
+    train1, test1 = split(E1x, 10)
+    train2, test2 = smart_random2(E2, 5, train1, test1)
